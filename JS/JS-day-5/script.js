dd487afa8616fc75d3b060ac30275c643241642d
virat-dagar <virat.hitaishi@gmail.com>
2026-01-23 11:31:10 +0530

    push

--- JS/JS-day-5/script.js
+++ JS/JS-day-5/script.js
@@ -1,5 +1,15 @@
-let sentence = 'i am a comma separated sentence';
-let words = sentence.split(' ');
-console.log(words);
-let words2 = sentence.split('a');
-console.log(words2)
+// let sentence = 'i am a comma separated sentence';
+// let words = sentence.split(' ');
+// console.log(words);
+// let words2 = sentence.split('a');
+// console.log(words2)
+
+
+
+
+// let str='tu meri me tera me tera tu meri ';
+// let timeR = str.split('r');
+// console.log(timeR.length - 1)
+
+
+
