61afb3fdd95bea589868bd45ea6783c9eea2525a
virat-dagar <virat.hitaishi@gmail.com>
2026-01-30 18:06:39 +0530

    mchanges to the previous commit of day 7

--- JS/JS-day-7/script.js
+++ JS/JS-day-7/script.js
@@ -1,14 +1,18 @@
-const heading = document.getElementById("myheading");
+//DOM TRAVERSAL
+//1. getElementById
+const heading=document.getElementById("myheading");
 console.log(heading);
+heading.style.color="tomato"
+heading.style.backgroundColor="yellow"
+heading.textContent="Hello Student!";
 
-heading.style.color ='tomato'
-heading.style.background = 'yellow'
-heading.textContent = 'hello students'
-
-const subheading = document.
-getElementsByClassName('sub-heading');
-console.log(subheading);
-
-for(let i = 0; i<subheading.length; i++){
-    subheading[i].style.color='red'
+//2. getElementByClassName
+const subHeading=document.getElementsByClassName("sub-heading")
+console.log(subHeading)
+for (let i=0;i<subHeading.length;i++){
+    subHeading[1].style.color="cyan"
 }
+//3.QUERY SELECTOR
+const content=document.querySelector("#content p")
+console.log(content);
+content.style.fontsize="40px"
